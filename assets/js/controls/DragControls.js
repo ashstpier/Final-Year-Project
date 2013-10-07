e65d056d1c484c506b8d3d285bdb428d67b591b9
Ashley St Pier <ashleystpier@gmail.com>
2013-10-07 20:58:34 +0100

    Re-ordered file structure

---
+++ assets/js/controls/DragControls.js
@@ -0,0 +1,203 @@
+/*
+ * DragControls dragging of three.js objects in any scene easily.
+ * 
+ * Usage: new DragControls(camera, objects, renderer.domElement);
+ *
+ * feature requests:
+ *  1. add rotation?
+ *  2. axis lock
+ *  3. inertia dragging
+ *  4. activate/deactivate? prevent propagation?
+ *
+ * issues:
+ *  deal with object parent's matrix?
+ *
+ * @author zz85 from https://github.com/zz85
+ * follow on http://twitter.com/blurspline
+ */
+THREE.DragControls = function(_camera, _objects, _domElement) {
+
+    if (_objects instanceof THREE.Scene) {
+        _objects = _objects.children;
+    }
+    var _projector = new THREE.Projector();
+
+    var _mouse = new THREE.Vector3(),
+        _offset = new THREE.Vector3();
+    var _selected;
+
+    var me = this;
+
+    _domElement.addEventListener('mousemove', onDocumentMouseMove, false);
+    _domElement.addEventListener('mousedown', onDocumentMouseDown, false);
+    _domElement.addEventListener('mouseup', onDocumentMouseUp, false);
+
+    // Enable Drag
+    this.enabled = true; 
+
+    /* Custom Event Handling */
+    var _listeners = {
+
+    };
+
+    this.on = function(event, handler) {
+        if (!_listeners[event]) _listeners[event] = [];
+
+        _listeners[event].push(handler);
+        return me;
+    };
+
+    this.off = function(event, handler) {
+        var l = _listeners[event];
+        if (!l) return me;
+
+        if (l.indexOf(handler)>-1) {
+            l.splice(handler, 1);
+        }
+
+        return me;
+
+    };
+
+    var notify = function(event, data, member) {
+        var l = _listeners[event];
+        if (!l) return;
+
+        if (!member) {
+            for (var i=0;i<l.length;i++) {
+                l[i](data);
+            }
+        }
+    };
+
+    this.setObjects = function(objects) {
+        if (objects instanceof THREE.Scene) {
+            _objects = objects.children;
+        } else {
+            _objects = objects;
+        }
+    };
+
+
+    // Drag constrains (eg. move along x-axis only, y only, x and y, or default xyz)
+    var moveX, moveY, moveZ;
+    moveX = moveY = moveZ = true;
+
+    this.constrains = function(xyz) {
+
+        if (xyz === undefined) 
+            xyz = 'xyz'; 
+
+        moveX = moveY = moveZ = false;
+
+        if (xyz.indexOf('x') > -1) {
+            moveX = true;
+        } 
+
+        if (xyz.indexOf('y') > -1) {
+            moveY = true;
+        } 
+
+        if (xyz.indexOf('z') > -1) {
+            moveZ = true;
+        } 
+
+        return this;
+
+    };
+
+
+    function onDocumentMouseMove(event) {
+
+        if (!me.enabled) return;
+
+        event.preventDefault();
+
+        _mouse.x = (event.clientX / _domElement.width) * 2 - 1;
+        _mouse.y = -(event.clientY / _domElement.height) * 2 + 1;
+
+        var ray = _projector.pickingRay(_mouse, _camera);
+
+        if (me.enabled && _selected) {
+            var targetPos = ray.direction.clone().multiplyScalar(_selected.distance).addSelf(ray.origin);
+            targetPos.subSelf(_offset);
+            // _selected.object.position.copy(targetPos.subSelf(_offset));
+
+            // Reverse Matrix?
+            // Hmm, quick hack - should do some kind of planar projection..
+            if (moveX) _selected.object.position.x = targetPos.x;
+            if (moveY) _selected.object.position.y = targetPos.y;
+            if (moveZ) _selected.object.position.z = targetPos.z;
+            notify('drag', _selected);
+            return;
+
+        }
+
+        var intersects = ray.intersectObjects(_objects);
+
+        if (intersects.length > 0) {
+
+            _domElement.style.cursor = 'pointer';
+
+        } else {
+
+            _domElement.style.cursor = 'auto';
+
+        }
+
+    }
+
+    function onDocumentMouseDown(event) {
+
+        if (!me.enabled) return;
+
+        event.preventDefault();
+
+        _mouse.x = (event.clientX / _domElement.width) * 2 - 1;
+        _mouse.y = -(event.clientY / _domElement.height) * 2 + 1;
+
+        var ray = _projector.pickingRay(_mouse, _camera);
+        var intersects = ray.intersectObjects(_objects);
+
+        var hit = intersects.length > 0;
+
+        if (hit) {
+            _selected = intersects[0];
+            
+            _offset.copy(_selected.point).subSelf(_selected.object.position);
+
+            _domElement.style.cursor = 'move';
+
+            _selected.hit = hit;
+
+            notify('dragstart', _selected);
+            notify('mousedown', _selected);
+        } else {
+            notify('mousedown', { hit: hit });
+        }
+
+    }
+
+    function onDocumentMouseUp(event) {
+
+        if (!me.enabled) return;
+
+        event.preventDefault();
+
+        var dragged = false;
+        if (_selected) {
+            dragged = true;
+            notify('dragend', _selected);
+
+            _selected = null;
+        }
+
+        _domElement.style.cursor = 'auto';
+
+        notify('mouseup', {
+            dragged: dragged
+        });
+
+    }
+
+}
---
+++ assets/js/core.js
@@ -0,0 +1,302 @@
+var container, stats;
+var camera, scene, projector, renderer, controls;
+var clickobjects = [];
+var hoverobjects = [];
+var sprites = [];
+
+init();
+animate();
+
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera.position.z = 450;
+	
+	//////////////////////////////////
+	//////// ORBIT CONTROLS //////////
+	//////////////////////////////////
+	
+	controls = new THREE.OrbitControls( camera );
+	controls.addEventListener( 'change', render );
+	controls.maxPolarAngle = Math.PI/4.5; 
+	controls.minDistance = 120;
+	controls.maxDistance = 300;
+
+	scene = new THREE.Scene();
+	scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+	
+	//////////////////////////////////
+	/////////// GEOMETRY /////////////
+	//////////////////////////////////
+	
+	loader = new THREE.JSONLoader();
+
+	loader.load( "assets/models/citybuildings.js", function( geometry ) {
+	
+		buildings = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial({
+				specular: '#ccc',
+				color: '#999',
+				emissive: '#000',
+				shininess: 100 
+			  }));
+		buildings.scale.set( 1.2, 1.2, 1.2 );
+		buildings.position.set( 0, -0.5, 0 );
+		scene.add( buildings );
+		
+		clickobjects.push( buildings );
+		hoverobjects.push( buildings );
+	} );
+	
+	texture = THREE.ImageUtils.loadTexture('assets/images/gmap.png', {}, function() {
+		renderer.render(scene);
+	})
+	
+	material = new THREE.MeshBasicMaterial({map: texture})
+	plane = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000, 8, 8 ), material );
+	plane.rotation.x += 270 * Math.PI / 180;
+	scene.add( plane );
+
+	
+	//////////////////////////////////
+	//////////// SPRITES /////////////
+	//////////////////////////////////
+
+	sprite1 = makeTextSprite( " Section 1 ", 
+		{ fontsize: 12, fontface: "Arial", borderColor: {r:0, g:0, b:0, a:1.0} } );
+	sprite1.position.set(-90,50,90);
+	scene.add( sprite1 );
+	
+	sprite2 = makeTextSprite( " Section 2 ", 
+		{ fontsize: 12, fontface: "Arial", borderColor: {r:0, g:0, b:0, a:1.0} } );
+	sprite2.position.set(90,50,90);
+	scene.add( sprite2 );
+	
+	sprite3 = makeTextSprite( " Section 3 ", 
+		{ fontsize: 12, fontface: "Arial", borderColor: {r:0, g:0, b:0, a:1.0} } );
+	sprite3.position.set(90,50,-90);
+	scene.add( sprite3 );
+	
+	sprite4 = makeTextSprite( " Section 4 ", 
+		{ fontsize: 12, fontface: "Arial", borderColor: {r:0, g:0, b:0, a:1.0} } );
+	sprite4.position.set(-90,50,-90);
+	scene.add( sprite4 );
+	
+	sprites.push( sprite1, sprite2, sprite3, sprite4 );
+	
+	
+	//////////////////////////////////
+	/////////// LIGHTS ///////////////
+	//////////////////////////////////
+	
+	var ambientLight = new THREE.AmbientLight(0x999999);
+	scene.add(ambientLight);
+  
+	// directional lighting
+	var light = new THREE.DirectionalLight(0xcccccc);
+	light.position.set(10, 200, 0).normalize();
+	scene.add(light);
+	
+	
+	//////////////////////////////////
+	/////////// RENDERER /////////////
+	//////////////////////////////////
+
+	projector = new THREE.Projector();
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	container.appendChild( renderer.domElement );
+
+	stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '0px';
+	container.appendChild( stats.domElement );
+
+	//document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+	//document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+	//
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+//////////////////////////////////
+///////// SPRITE BUILDER /////////
+//////////////////////////////////
+
+function makeTextSprite( message, parameters )
+{
+	if ( parameters === undefined ) parameters = {};
+	
+	var fontface = parameters.hasOwnProperty("fontface") ? 
+		parameters["fontface"] : "Arial";
+	
+	var fontsize = parameters.hasOwnProperty("fontsize") ? 
+		parameters["fontsize"] : 18;
+	
+	var borderThickness = parameters.hasOwnProperty("borderThickness") ? 
+		parameters["borderThickness"] : 1;
+	
+	var borderColor = parameters.hasOwnProperty("borderColor") ?
+		parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+	
+	var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+		parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+
+	var spriteAlignment = THREE.SpriteAlignment.topLeft;
+		
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	context.font = fontsize + "px " + fontface;
+	
+	// get size data (height depends only on font size)
+	var metrics = context.measureText( message );
+	var textWidth = metrics.width;
+	
+	// background color
+	context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+								  + backgroundColor.b + "," + backgroundColor.a + ")";
+	// border color
+	context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+								  + borderColor.b + "," + borderColor.a + ")";
+
+	context.lineWidth = borderThickness;
+	roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+	// 1.4 is extra height factor for text below baseline: g,j,p,q.
+	
+	// text color
+	context.fillStyle = "rgba(0, 0, 0, 1.0)";
+
+	context.fillText( message, borderThickness, fontsize + borderThickness);
+	
+	// canvas contents will be used for a texture
+	var texture = new THREE.Texture(canvas) 
+	texture.needsUpdate = true;
+
+	var spriteMaterial = new THREE.SpriteMaterial( 
+		{ map: texture, useScreenCoordinates: false, alignment: spriteAlignment } );
+	var sprite = new THREE.Sprite( spriteMaterial );
+	sprite.scale.set(100,50,1.0);
+	return sprite;	
+}
+
+//////////////////////////////////
+//////// ROUND RECTANGLES ////////
+//////////////////////////////////
+
+function roundRect(ctx, x, y, w, h, r) 
+{
+	ctx.beginPath();
+	ctx.moveTo(x+r, y);
+	ctx.lineTo(x+w-r, y);
+	ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+	ctx.lineTo(x+w, y+h-r);
+	ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+	ctx.lineTo(x+r, y+h);
+	ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+	ctx.lineTo(x, y+r);
+	ctx.quadraticCurveTo(x, y, x+r, y);
+	ctx.closePath();
+	ctx.fill();
+	ctx.stroke();   
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+//////////////////////////////////
+////////// CLICK EVENT ///////////
+//////////////////////////////////
+
+function onDocumentMouseUp( event ) {
+
+	event.preventDefault();
+
+	var vector = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
+	projector.unprojectVector( vector, camera );
+
+	var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+	var intersects = raycaster.intersectObjects( clickobjects );
+
+	if ( intersects.length > 0 ) {
+		
+		if ( intersects[0] ){
+			
+			var time = 2000;
+			
+			if ( intersects[0].object.id == clickobjects[0].id ){
+				new TWEEN.Tween( camera.position ).to( {
+				x: intersects[0].object.position.x,
+				z: intersects[0].object.position.z + 150 }, time )
+			.easing( TWEEN.Easing.Quadratic.InOut).start();
+			}
+			
+			new TWEEN.Tween( controls.center ).to( {
+				x: intersects[0].object.position.x,
+				y: 0,
+				z: intersects[0].object.position.z }, time )
+			.easing( TWEEN.Easing.Quadratic.InOut).start();
+			
+		}
+	}
+}
+
+//////////////////////////////////
+//////// MOUSEMOVE EVENT /////////
+//////////////////////////////////
+
+function onDocumentMouseMove( event ) {
+
+	event.preventDefault();
+
+	var vector = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
+	projector.unprojectVector( vector, camera );
+
+	var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+	var intersects = raycaster.intersectObjects( hoverobjects );
+
+	if (intersects[ 0 ]) {
+		//intersects[ 0 ].object.material.color.setHex( 0xcc0000 );
+		$('canvas').css('cursor','pointer')
+		
+	}else{
+		$('canvas').css('cursor','auto')
+		//for (var i=0;i<hoverobjects.length;i++){ 
+			//hoverobjects[ i ].material.color.setHex( 0xffffff );}
+	}
+	
+}
+
+
+//
+
+function animate() {
+	
+	TWEEN.update();
+	requestAnimationFrame( animate );
+	controls.update();
+	
+	render();
+	stats.update();
+	
+}
+
+
+function render() {
+	
+	renderer.render( scene, camera );
+
+}
