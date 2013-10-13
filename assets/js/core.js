7a647f1f39dad8547753c21916f8c860aa844b94
Ashley St Pier <ashleystpier@gmail.com>
2013-10-13 22:24:52 +0100

    no message

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,59 +1,75 @@
-var container, stats;
-var camera, scene, projector, renderer, controls;
+var container, stats, camera, scene, renderer, projector, controls, group;
+var depthMaterial, depthTarget, composer;
 var clickobjects = [], hoverobjects = [], sprites = [];
 
 init();
 animate();
 
 function init() {
-
+	
 	container = document.createElement( 'div' );
 	document.body.appendChild( container );
-
-	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera.position.z = 450;
+	
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 10, 1000 );
+	camera.position.z = 500;
 	
 	//////////////////////////////////
 	//////// ORBIT CONTROLS //////////
 	//////////////////////////////////
-	
+
 	controls = new THREE.OrbitControls( camera );
 	controls.addEventListener( 'change', render );
-	controls.maxPolarAngle = Math.PI/4; 
-	controls.minDistance = 120;
+	controls.maxPolarAngle = Math.PI/4.5; 
+	controls.minDistance = 200;
 	controls.maxDistance = 300;
 
 	scene = new THREE.Scene();
-	scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
-	
+	scene.fog = new THREE.Fog( 0xeeeeee, 0.002 );
+
 	//////////////////////////////////
 	/////////// GEOMETRY /////////////
 	//////////////////////////////////
+		
+	group = new THREE.Object3D();
+	scene.add( group );
+	
+	var material = new THREE.MeshDepthMaterial( { side: THREE.DoubleSide, overdraw: true } );
 	
 	loader = new THREE.JSONLoader();
-	
-	var depthmaterial = new THREE.MeshDepthMaterial( { side: THREE.DoubleSide, overdraw: true } );
 
 	loader.load( "assets/models/citybuildings.js", function( geometry ) {
 
-		buildings = new THREE.Mesh( geometry, depthmaterial );
-		buildings.scale.set( 1.2, 1.2, 1.2 );
-		buildings.position.set( 0, -0.5, 0 );
-		scene.add( buildings );
+		buildings = new THREE.Mesh( geometry, material );
+		buildings.scale.set( 1.5, 1.5, 1.5 );
+		buildings.position.set( -100, -0.5, -100 );
+		group.add( buildings );
 		
-		clickobjects.push( buildings );
-		hoverobjects.push( buildings );
+		buildings2 = new THREE.Mesh( geometry, material );
+		buildings2.scale.set( 1.5, 1.5, 1.5 );
+		buildings2.position.set( 100, -0.5, 100 );
+		group.add( buildings2 );
+		
+		buildings3 = new THREE.Mesh( geometry, material );
+		buildings3.scale.set( 1.5, 1.5, 1.5 );
+		buildings3.position.set( -100, -0.5, 100 );
+		group.add( buildings3 );
+		
+		buildings4 = new THREE.Mesh( geometry, material );
+		buildings4.scale.set( 1.5, 1.5, 1.5 );
+		buildings4.position.set( 100, -0.5, -100 );
+		group.add( buildings4 );
+		
+		clickobjects.push( buildings, buildings2, buildings3, buildings4 );
+		hoverobjects.push( buildings, buildings2, buildings3, buildings4 );
 	} );
 	
 	texture = THREE.ImageUtils.loadTexture('assets/images/gmap.png', {}, function() {
 		renderer.render(scene);
 	})
-	
-	material = new THREE.MeshBasicMaterial({map: texture})
-	plane = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000, 8, 8 ), material );
+	mapmaterial = new THREE.MeshBasicMaterial({map: texture})
+	plane = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000, 8, 8 ), mapmaterial );
 	plane.rotation.x += 270 * Math.PI / 180;
 	scene.add( plane );
-
 	
 	//////////////////////////////////
 	//////////// SPRITES /////////////
@@ -81,24 +97,10 @@ function init() {
 	
 	sprites.push( sprite1, sprite2, sprite3, sprite4 );
 	
-	
 	//////////////////////////////////
-	/////////// LIGHTS ///////////////
+	//////////// RENDERER ////////////
 	//////////////////////////////////
 	
-	var ambientLight = new THREE.AmbientLight(0x999999);
-	scene.add(ambientLight);
-  
-	// directional lighting
-	var light = new THREE.DirectionalLight(0xcccccc);
-	light.position.set(10, 200, 0).normalize();
-	scene.add(light);
-	
-	
-	//////////////////////////////////
-	/////////// RENDERER /////////////
-	//////////////////////////////////
-
 	projector = new THREE.Projector();
 
 	renderer = new THREE.WebGLRenderer();
@@ -109,6 +111,7 @@ function init() {
 	stats = new Stats();
 	stats.domElement.style.position = 'absolute';
 	stats.domElement.style.top = '0px';
+	stats.domElement.style.right = '0px';
 	container.appendChild( stats.domElement );
 
 	document.addEventListener( 'mouseup', onDocumentMouseUp, false );
@@ -118,6 +121,35 @@ function init() {
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
+	/*			
+	//////////////////////////////////
+	///////////// DEPTH //////////////
+	//////////////////////////////////
+	
+	var depthShader = THREE.ShaderLib[ "depthRGBA" ];
+	var depthUniforms = THREE.UniformsUtils.clone( depthShader.uniforms );
+
+	depthMaterial = new THREE.ShaderMaterial( { fragmentShader: depthShader.fragmentShader, vertexShader: depthShader.vertexShader, uniforms: depthUniforms } );
+	depthMaterial.blending = THREE.NoBlending;
+
+	//////////////////////////////////
+	////////////// SSAO //////////////
+	//////////////////////////////////
+	
+	composer = new THREE.EffectComposer( renderer );
+	composer.addPass( new THREE.RenderPass( scene, camera ) );
+
+	depthTarget = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.NearestFilter, magFilter: THREE.NearestFilter, format: THREE.RGBAFormat } );
+	
+	var effect = new THREE.ShaderPass( THREE.SSAOShader );
+	effect.uniforms[ 'tDepth' ].value = depthTarget;
+	effect.uniforms[ 'size' ].value.set( window.innerWidth, window.innerHeight );
+	effect.uniforms[ 'cameraNear' ].value = camera.near;
+	effect.uniforms[ 'cameraFar' ].value = camera.far;
+	effect.renderToScreen = true;
+	composer.addPass( effect );
+	*/
+	
 }
 
 //////////////////////////////////
@@ -177,7 +209,7 @@ function makeTextSprite( message, parameters )
 		{ map: texture, useScreenCoordinates: false, alignment: spriteAlignment } );
 	var sprite = new THREE.Sprite( spriteMaterial );
 	sprite.scale.set(100,50,1.0);
-	return sprite;	
+	return sprite;
 }
 
 //////////////////////////////////
@@ -201,21 +233,12 @@ function roundRect(ctx, x, y, w, h, r)
 	ctx.stroke();   
 }
 
-function onWindowResize() {
-
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-
-	renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
 //////////////////////////////////
 ////////// CLICK EVENT ///////////
 //////////////////////////////////
 
 function onDocumentMouseUp( event ) {
-
+	
 	event.preventDefault();
 
 	var vector = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
@@ -224,28 +247,59 @@ function onDocumentMouseUp( event ) {
 	var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
 
 	var intersects = raycaster.intersectObjects( clickobjects );
+	
+	/////////// AJAX call from XML ///////////
+	
+	var slide = 1;
+	
+	$("#slidepanel").empty();
+	
+	$.ajax({
+		type: "GET",
+		url: "assets/slides.xml",
+		dataType: "xml",
+		success: function(xml) {
+			$(xml).find('slide'+slide).each(function(){
+				var id = $(this).attr('id');
+				var title = $(this).find('title').text();
+				$('<section id="'+id+'"></section>').html('<h2>'+title+'</h2>').appendTo('#slidepanel');
+				var content = $(this).find('content');
+				$(content).appendTo('#'+id);
+			});
+		}
+	});
 
 	if ( intersects.length > 0 ) {
 		
-		if ( intersects[0] ){
+		var time = 1000;
+		
+		if ( intersects[0].object.id == clickobjects[0].id ){
+			slide = 1;
+		}
+		else if ( intersects[0].object.id == clickobjects[1].id ){
+			slide = 2;
+		}
+		else if ( intersects[0].object.id == clickobjects[2].id ){
+			slide = 3;
+		}
+		else if ( intersects[0].object.id == clickobjects[3].id ){
+			slide = 4;
+		}
+		
+		$("#slidepanel").toggle("slide", {direction:'left'});
 			
-			var time = 2000;
-			
-			if ( intersects[0].object.id == clickobjects[0].id ){
-				new TWEEN.Tween( camera.position ).to( {
+			new TWEEN.Tween( camera.position ).to( {
 				x: intersects[0].object.position.x,
 				z: intersects[0].object.position.z + 150 }, time )
 			.easing( TWEEN.Easing.Quadratic.InOut).start();
-			}
 			
 			new TWEEN.Tween( controls.center ).to( {
 				x: intersects[0].object.position.x,
 				y: 0,
 				z: intersects[0].object.position.z }, time )
 			.easing( TWEEN.Easing.Quadratic.InOut).start();
-			
-		}
 	}
+	
 }
 
 //////////////////////////////////
@@ -263,7 +317,7 @@ function onDocumentMouseMove( event ) {
 
 	var intersects = raycaster.intersectObjects( hoverobjects );
 
-	if (intersects[ 0 ]) {
+	if (intersects [ 0 ] ) {
 		//intersects[ 0 ].object.material.color.setHex( 0xcc0000 );
 		$('canvas').css('cursor','pointer')
 		
@@ -276,22 +330,32 @@ function onDocumentMouseMove( event ) {
 }
 
 
-//
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
 
 function animate() {
 	
 	TWEEN.update();
 	requestAnimationFrame( animate );
 	controls.update();
-	
-	render();
 	stats.update();
 	
-}
+	//scene.overrideMaterial = depthMaterial;
+	render();
 
+	//scene.overrideMaterial = null;
+	//composer.render();
 
-function render() {
 	
-	renderer.render( scene, camera );
+}
+function render() {
+
+	renderer.render( scene, camera/*, depthTarget*/ );
 
 }
